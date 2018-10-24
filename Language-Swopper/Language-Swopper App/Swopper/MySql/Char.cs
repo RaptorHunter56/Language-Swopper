a7dfc8b32fb8d10419b080166ac06b30cc4b6d57
steven brown <sb.nb.lego@Gmail.com>
2018-10-25 10:12:12 +1300

    SB - [041]
    Char For MySQL done

---
+++ Language-Swopper/Language-Swopper App/Swopper/MySql/Char.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System;
+using Base;
+
+namespace LswMySql
+{
+    public static class lswCharPath
+    {
+        public static string Write(object One)
+        {
+            LsChar Two = (LsChar)One;
+            foreach (Prefix prefix in Two.Prefixes)
+            {
+                if (prefix == Prefix.@public)
+                {
+                    Two.Name = "@" + Two.Name;
+                }
+            }
+            string temp = "";
+            if (Two.ValueType)
+                temp = "SET " + Two.Name + " = " + Two.ValueT + ";";
+            else
+                temp = "SET " + Two.Name + " = '" + Two.Value + "';";
+            return temp;
+        }
+
+        public static LsChar Read(string One)
+        {
+            string Two = One.Split('=')[0].Split(' ')[1].Trim();
+            string Three = One.Split('=')[1].Trim().Trim(';').Trim();
+            List<Prefix> prefixes = new List<Prefix>();
+            if (Two[0] == '@')
+                prefixes.Add(Prefix.@public);
+            else
+                prefixes.Add(Prefix.@private);
+            LsChar Four;
+            if (Three == "'\\''")
+                Four = new LsChar(Two, '\'', prefixes);
+            else if (Three[0] == '\'' && Three[2] == '\'')
+                Four = new LsChar(Two, Three.Substring(1, 1).ToCharArray()[0], prefixes);
+            else
+                Four = new LsChar(Two, Three, prefixes);
+            return Four;
+        }
+    }
+}
+
+//########
+//',255|165|42|42.StartToEnd
--- Language-Swopper/Language-Swopper App/Swopper/MySql/Controler.cs
+++ Language-Swopper/Language-Swopper App/Swopper/MySql/Controler.cs
@@ -27,11 +27,14 @@ namespace LswMySql
             {
 
                 Regex stringrgx = new Regex(@"(s|S)(e|E)(t|T) .+ {0,}= {0,}'(([^']{0,}[\\]'[^'\\]{0,}){1,}|([^']{0,}''[^']{0,}){1,}|[^']{1,})';{0,1}");
+                Regex chargrgx = new Regex(@"(s|S)(e|E)(t|T) .+ {0,}= {0,}'(([\\]'){1}|[^']{1})';{0,1}");
                 Regex boolrgx = new Regex(@"(s|S)(e|E)(t|T) .+ {0,}= {0,}((t|T)(r|R)(u|U)(e|E)|(f|F)(a|A)(l|L)(s|S)(e|E));{0,1}");
 
-                if (stringrgx.Match(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()).Success)
+                if (chargrgx.Match(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()).Success)
+                    Return.Bases.Add(lswCharPath.Read(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()));
+                else if (stringrgx.Match(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()).Success)
                     Return.Bases.Add(lswStringPath.Read(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()));
-                else if(boolrgx.Match(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()).Success)
+                else if (boolrgx.Match(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()).Success)
                     Return.Bases.Add(lswBoolPath.Read(MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd()));
                 else
                     Return.Bases.Add(new LsName() { Name = MySqlPositionRef.InLine[MySqlPositionRef.Position].TrimEnd(), Lanaguage = "C#" });
@@ -72,6 +75,8 @@ namespace LswMySql
                         Return += lswStringPath.Write(item) + " {LsString} " + "\r";
                     else if (((lsBase)item).lsType == "LsBool")
                         Return += lswBoolPath.Write(item) + " {LsBool} " + "\r";
+                    else if (((lsBase)item).lsType == "LsChar")
+                        Return += lswCharPath.Write(item) + " {LsChar} " + "\r";
                     else if (((lsBase)item).lsType == "LsName")
                         try { Return += ((LsName)item).Lanaguage + " Doesn't Have a conversion file for this." + "\r"; } catch { Return += "{No_Type}" + "\r"; }
                     else
