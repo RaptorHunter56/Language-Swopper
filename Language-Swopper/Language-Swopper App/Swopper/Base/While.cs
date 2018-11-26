646b6b9d3e9c960e1ea3db49af29b7c2ff5ec6f8
steven brown <sb.nb.lego@Gmail.com>
2018-11-26 17:08:19 +1300

    SB - [078]
    While Done

---
+++ Language-Swopper/Language-Swopper App/Swopper/Base/While.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Base
+{
+    public class LsWhile : lsBase
+    {
+        public LsWhile()
+        { lsType = "LsWhile"; }
+        public LsBracket Bracket;
+        public List<lsBase> InerLines = new List<lsBase>();
+        public int Tabindex = 0;
+        public bool EndIf = false;
+    }
+}
